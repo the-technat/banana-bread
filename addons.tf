385c4abf56fe2d3e02b81cb62f2ddcd6df662a27
Nathanael Liechti <technat@technat.ch>
2023-03-22 16:11:07 +0000

    feat: add argocd

--- addons.tf
+++ addons.tf
@@ -1,3 +1,24 @@
+##########
+# Argo CD
+##########
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  version          = "5.x.x"
+  namespace        = "argocd"
+  create_namespace = true
+
+  values = [
+    "${file("helm_values/argocd_values.yaml")}"
+  ]
+
+  depends_on = [
+    module.eks,
+    helm_release.cilium
+  ]
+}
+
 # module "eks_blueprints_kubernetes_addons" {
 #   source = "github.com/aws-ia/terraform-aws-eks-blueprints/modules/kubernetes-addons"
 
