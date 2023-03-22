fedfffc00a372cfb2a06b7b93c0fc3e1a3fbe092
Nathanael Liechti <technat@technat.ch>
2023-03-22 13:47:36 +0100

    chore: some refactorings

--- eks.tf
+++ eks.tf
@@ -2,33 +2,38 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 19.9"
 
-  cluster_name                   = local.name
-  cluster_version                = local.cluster_version
-  cluster_endpoint_public_access = true
+  cluster_name    = local.name
+  cluster_version = local.cluster_version
 
   cluster_addons = {
     coredns    = {}
     kube-proxy = {}
+    vpc-cni    = {}
   }
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  # Networking
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
 
+  # IAM
   manage_aws_auth_configmap = true
   aws_auth_users            = local.aws_auth_users
+
+  # Data-plane
   eks_managed_node_groups = {
     minions = {
       name            = "minions"
       use_name_prefix = true
-      capacity_type   = "SPOT"
-      instance_types  = local.instance_types
+
+      capacity_type  = "SPOT"
+      instance_types = local.instance_types
 
       update_config = {
         max_unavailable_percentage = 33
       }
 
       ami_type = local.ami_type
-      platform = local.platform
 
       min_size     = local.min_size
       max_size     = local.max_size
--- locals.tf
+++ locals.tf
@@ -3,14 +3,13 @@ locals {
   cluster_version = "1.25"
   region          = "eu-central-2"
 
-  instance_types = ["t3.medium"]
+  instance_types = ["t3a.medium", "t3.medium"]
   min_size       = 0
   max_size       = 3
   desired_size   = 1
-  ami_type       = "BOTTLEROCKET_x86_64"
-  platform       = "bottlerocket"
+  ami_type       = "AL2_x86_64"
 
-  vpc_cidr = "10.0.0.0/16"
+  vpc_cidr = "10.123.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
   aws_auth_users = [
