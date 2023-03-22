123b38b1abe4b863511dd33f8935992fbe163b88
Nathanael Liechti <technat@technat.ch>
2023-03-22 15:41:42 +0000

    fix: update cilium to 1.12

--- eks.tf
+++ eks.tf
@@ -135,7 +135,7 @@ resource "helm_release" "cilium" {
   name       = "cilium"
   repository = "https://helm.cilium.io"
   chart      = "cilium"
-  version    = "1.11.4"
+  version    = "1.12.5"
   namespace  = "kube-system"
   wait = true
 
