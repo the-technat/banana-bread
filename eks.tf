aa3a79292b323a9ae30cffa8ceba5dd35764b162
Nathanael Liechti <technat@technat.ch>
2023-03-24 12:32:28 +0000

    feat: one node group per AZ

--- eks.tf
+++ eks.tf
@@ -40,42 +40,52 @@ module "eks" {
     },
   ]
 
+  eks_managed_node_group_defaults = {
+    use_name_prefix = true
+
+    capacity_type  = "SPOT"
+    ami_type       = "AL2_x86_64"
+    ami_id         = "ami-029bc1687a2afeb19" # amazon-aws-eks-node-1.24-v*
+    instance_types = ["t3a.xlarge", "t3.xlarge", "t2.xlarge"]
+
+    min_size     = 0
+    max_size     = 10
+    desired_size = 3
+
+    network_interfaces = [
+      {
+        associate_public_ip_address = false
+        delete_on_termination       = true
+        security_groups             = [module.eks.cluster_primary_security_group_id, aws_security_group.eks_cluster.id]
+      }
+    ]
+
+    iam_role_additional_policies = [
+      "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
+    ]
+
+    enable_bootstrap_user_data = true
+    taints = [
+      # will be removed by cilium once initialized
+      {
+        key    = "node.cilium.io/agent-not-ready"
+        value  = "true"
+        effect = "NO_EXECUTE"
+      }
+    ]
+  }
   eks_managed_node_groups = {
-    minions = {
-      name            = "minions"
-      use_name_prefix = true
-
-      capacity_type  = "SPOT"
-      ami_type       = "AL2_x86_64"
-      ami_id         = "ami-029bc1687a2afeb19" # amazon-aws-eks-node-1.24-v*
-      instance_types = ["t3a.xlarge", "t3.xlarge", "t2.xlarge"]
-
-      min_size     = 0
-      max_size     = 10
-      desired_size = 3
-
-      subnet_ids = module.vpc.private_subnets
-      network_interfaces = [
-        {
-          associate_public_ip_address = false
-          delete_on_termination       = true
-          security_groups             = [module.eks.cluster_primary_security_group_id, aws_security_group.eks_cluster.id]
-        }
-      ]
-
-      iam_role_additional_policies = [
-        "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
-      ]
-
-      enable_bootstrap_user_data = true
-      taints = [
-        # will be removed by cilium once initialized
-        {
-          key    = "node.cilium.io/agent-not-ready"
-          value  = "true"
-          effect = "NO_EXECUTE"
-        }
-      ]
+    minions1 = {
+      name       = "minions1"
+      subnet_ids = [module.vpc.private_subnets[0]]
+    }
+    minions2 = {
+      name       = "minions2"
+      subnet_ids = [module.vpc.private_subnets[1]]
+    }
+    minions3 = {
+      name       = "minions3"
+      subnet_ids = [module.vpc.private_subnets[2]]
     }
   }
 
