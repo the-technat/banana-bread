86e145f6b7bd4205cc45a983a641e5a85cdd462e
Nathanael Liechti <technat@technat.ch>
2023-06-08 14:22:14 +0000

    feat: add metrics-server

---
+++ metrics_server.tf
@@ -0,0 +1,21 @@
+##########
+# Metrics Server
+##########
+resource "helm_release" "metrics_server" {
+  name             = "argocd"
+  repository       = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart            = "metrics-server"
+  version          = "3.x.x"
+  namespace        = "metrics-server"
+  create_namespace = true
+  values = [
+    templatefile("${path.module}/helm_values/metrics_server.yaml", {
+    })
+  ]
+
+  depends_on = [
+    module.eks,
+    helm_release.cilium,
+    kubernetes_priority_class_v1.infra
+  ]
+}
